deca38ad3653bf26f039c48e21536992d455f4bb
vSeasky <2544907929@qq.com>
2023-02-17 23:27:43 +0800

    remove k2

--- software/k210-yolo2/build/lib/CMakeFiles/kendryte.dir/bsp/crt.S.obj.d
+++
@@ -1,3 +0,0 @@
-lib/CMakeFiles/kendryte.dir/bsp/crt.S.obj: \
- E:\github\riscv-k210\software\k210-yolo\lib\bsp\crt.S \
- E:\github\riscv-k210\software\k210-yolo\lib\bsp\include/encoding.h
--- software/k210-yolo2/build/lib/CMakeFiles/kendryte.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kendryte.
-# This may be replaced when dependencies are built.
